17e9d6a864b3b9d66129ebc6fd883a463996cd16
preciousakura <isawithans@gmail.com>
2020-10-08 18:13:55 -0300

    atualização do dia 08/10

--- jogodavelha.py
+++ jogodavelha.py
@@ -2,23 +2,49 @@
 # Disciplina Fundamentos de Programação – Semestre 2020.1
 # Isabel Cristina de Oliveira Lopes
 # Matrícula: 493948
-
-jogar = input("Escolha sua peça, Jogador 1 [X/O]: ").lower()
-while jogar not in "xo":
-    jogar = input("Peça inválida, Jogador 1, digite outra: ").lower()
-    
-if jogar[0] == "x":
-    rodada = True
-    
-elif jogar[0]=="o":
-    rodada = False
-
-partida = 0
- 
 def matriz(linhas, colunas, val_inic): #Criar matriz
     mat = [[val_inic] * colunas for _ in range(linhas)]
     return mat
-    
+
+def menu():
+    game = 0
+    playA = 0
+    playB = 0
+    keep = True
+    rodada = True
+    while keep: 
+        jogo = matriz(3,3,-1) #Criação da matriz principal do jogo da velha 
+        continuar = input("0. Sair \n"+
+                              "1. Jogar \n") 
+                              
+        while(continuar != '0' and continuar != '1'):
+            continuar = input("Opção Inválida! Digite novamente: ")
+                        
+        if continuar != "0":
+            jogar = input("Escolha sua peça, Jogador 1 [X/O]: ").lower()
+            while jogar != 'x' and jogar!='o':
+                jogar = input("Peça inválida, digite outra: ").lower()        
+            if jogar[0] == "x":
+                rodada = True            
+            elif jogar[0]=="o":
+                rodada = False
+                    
+            print()
+            print("PLACAR:")       
+            print("Jogador 1:", playA)
+            print("Jogador 2:", playB)
+            print()  
+            print("PARTIDA:", game+1)
+            partida = 0   
+            if fazer_jogada(jogo, rodada, partida) == 1:
+                playA += 1
+            else:
+                playB += 1      
+            game+=1       
+        else:
+            print("Saindo...")
+            keep = False                     
+
 def mostrar_jogo(jogo_atual): #Mostra a matriz do jogo da velha com as jogadas atualizadas
     print("  {:<4}{:<4}{:<1}".format("A","B","C")) #Printa as colunas no inicio
     for i in range(len(jogo_atual)): #For para as linhas
@@ -49,30 +75,44 @@ def mostrar_jogo(jogo_atual): #Mostra a matriz do jogo da velha com as jogadas a
 def verifica_jogada(peca, matriz): 
     jogada = True 
     while jogada: #Condição para o while rodar
-        if len(peca) == 3 and peca: #se digitarem mais de 3 caracteres, pedirei outra jogada
-            if not(peca[0] not in "aAbBcC") and (peca[1] == ",") and not(peca[2] not in '123'): #se não tiver o formato [letra,numero] pedirei outra jogada
-                coluna, linha = peca.split(",") #fatia a jogada em letra e numero
-                linha = int(linha) #tranforma o valor da coluna em inteiro
-                if coluna == 'a': #converte de letra para número para ser usado na matriz
-                    coluna = int(0)
-                elif coluna == 'b':
-                    coluna = int(1)
-                elif coluna == 'c':
-                    coluna = int(2)
-                    
-                if matriz[linha-1][coluna] != -1: #verifica se a jogada já foi feita
-                    peca = input("Você já fez essa jogada! Digite outra [LETRA,NUMERO]: ").lower()
-                    
-                else:
-                    return linha-1, coluna #retorna a linha e a coluna convertida
-                    jogada = False #para o while
-            else:
-                peca = input("Coordenadas inválidas! Digite novamente [LETRA,NUMERO]: ").lower()
-        else:
+        entrada = []
+        virgula = 0
+        for x in peca:
+            if x != " " and x != ",":
+                entrada.append(x)  
+            if x == ",":
+                virgula += 1
+            
+            
+        if len(entrada) != 2 or virgula > 1:
             peca = input("Coordenadas inválidas! Digite novamente [LETRA,NUMERO]: ").lower()
+        else:       
+            coluna = entrada[0]
+            linha = entrada[1]
+            
+            if coluna not in "abc" or linha not in "123":
+                peca = input("Coordenadas inválidas! Digite novamente [LETRA,NUMERO]: ").lower()
+                
+            else:
+                linha = int(entrada[1])
+                if (coluna=='a' or coluna=='b' or coluna=='c') and (linha <= 3 and linha>= 0):         
+                    if coluna == 'a': #converte de letra para número para ser usado na matriz
+                        coluna = int(0)
+                    elif coluna == 'b':
+                        coluna = int(1)
+                    elif coluna == 'c':
+                        coluna = int(2)
+                        
+                    if matriz[linha-1][coluna] != -1: #verifica se a jogada já foi feita
+                        peca = input("Você já fez essa jogada! Digite outra [LETRA,NUMERO]: ").lower()
+                    
+                    else:
+                        return linha-1, coluna #retorna a linha e a coluna convertida
+                        jogada = False #para o while                   
+                else: 
+                    peca = input("Coordenadas inválidas! Digite novamente [LETRA,NUMERO]: ").lower()
         
 def fazer_jogada(matriz, rodada, partida): 
-
     ganhar = True
     while(ganhar):
         print()
@@ -96,42 +136,27 @@ def fazer_jogada(matriz, rodada, partida):
         l = 0
         c = 0
         d = 0
-        
-        if rodada:
-            matriz[linha][coluna] = True
-            for x in range(3):
-                if matriz[linha][x] == True:
-                    l += 1
-            
-                if matriz[x][coluna] == True:
-                    c += 1
+        ds = 0
+        cont = 2
+        matriz[linha][coluna] = rodada
+        for x in range(3):
+            #checando linhas
+            if matriz[linha][x] == rodada:
+                l += 1
+            #checando colunas
+            if matriz[x][coluna] == rodada:
+                c += 1
+             #checando diagonal primária      
+            if matriz[x][x] == rodada:
+                d += 1 
+            #checando diagonal secundária
+            if matriz[cont][x] == rodada:
+                ds += 1
+            cont -= 1
                     
-            for i in range(len(matriz)): 
-                for j in range(len(matriz[0])): 
-                    if i==j:
-                        if matriz[i][j] == True:
-                            d += 1
-            
-            rodada = False
+        rodada = not(rodada)
         
-        elif not(rodada):
-            matriz[linha][coluna] = False
-            for x in range(3):
-                if matriz[linha][x] == False:
-                    l += 1
-            
-                if matriz[x][coluna] == False:
-                    c += 1
-                    
-            for i in range(len(matriz)): 
-                for j in range(len(matriz[0])): 
-                    if i==j:
-                        if matriz[i][j] == False:
-                            d += 1
-            
-            rodada = True
-        
-        if l == 3 or c == 3 or d == 3:
+        if l == 3 or c == 3 or d == 3 or ds == 3:
             print()
             print()
             print("-"*10, end=' ')
@@ -142,6 +167,7 @@ def fazer_jogada(matriz, rodada, partida):
             mostrar_jogo(matriz)
             print()
             print()
+            return jogar
             ganhar = False
             
         elif(partida > 8):
@@ -156,20 +182,7 @@ def fazer_jogada(matriz, rodada, partida):
             print()
             print()
             ganhar = False
-        
-    
+  
+  
 
-
-        
-    
-                
-        
-         
-
-jogo = matriz(3,3,-1) #Criação da matriz principal do jogo da velha  
-fazer_jogada(jogo, rodada, partida) 
-
-
-
-
-
+menu() 
---
+++ teste.py
@@ -0,0 +1,16 @@
+def acharLinha(entrada, item):
+    primeiro = 0
+    ult = len(entrada)-1
+    found = False
+    while primeiro <= ult and not found:
+        meio = (primeiro + ult)//2
+        if entrada[meio] == item:
+            found = True
+        else:
+            if item<entrada[meio]:
+                ult = meio-1
+            else:
+                primeiro = meio+1
+    return item
+    
+    
